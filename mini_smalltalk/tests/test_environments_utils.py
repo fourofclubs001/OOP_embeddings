52899c4464ed5b43c7d30603118521396e787ecf
Lucas Vitali <lucasvitali001@gmail.com>
2025-11-11 20:16:37 -0300

    refactor: can get trace test

--- mini_smalltalk/tests/test_environments_utils.py
+++ mini_smalltalk/tests/test_environments_utils.py
@@ -37,7 +37,7 @@ class EnvironmentUtilsTest(unittest.TestCase):
         
         self.assertSetEqual(set(expected_pairs), set(class_method_pairs))
 
-    def test_can_return_trace(self):
+    def define_create_identity_dictionary(self):
 
         self.environment.define_method(
             "Dictionary", "create_identity_dictionary", ["key_and_value"],
@@ -45,18 +45,28 @@ class EnvironmentUtilsTest(unittest.TestCase):
              ("dictionary", "set", ["key_and_value", "key_and_value"], "dictionary")],
               "dictionary"
         )
-        
+
+    def define_dictionary_instance_and_key(self):
+
         self.environment.send_message("Dictionary", "new", [], "main_dictionary")
         self.environment.send_message("String", "new", ["first_key"], "main_dictionary_first_key")
 
-        trace = self.environment.send_message(
+    def send_message_create_identity_dictionary(self, trace, base_case):
+
+        return self.environment.send_message(
             
             "main_dictionary", 
             "create_identity_dictionary", 
             ["main_dictionary_first_key"], 
             "main_dictionary", 
-            trace = True, base_case = True
+            trace, base_case
         )
+
+    def test_can_get_trace(self):
+
+        self.define_create_identity_dictionary()
+        self.define_dictionary_instance_and_key()
+        trace = self.send_message_create_identity_dictionary(trace=True, base_case=True)
         
         expected_trace = [
 
@@ -67,3 +77,6 @@ class EnvironmentUtilsTest(unittest.TestCase):
         ]
 
         self.assertListEqual(trace, expected_trace)
+
+    def test_can_get_multilevel_trace(self): pass
+
