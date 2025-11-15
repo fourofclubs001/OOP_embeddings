a5a12582517e18d808c1ca0a300c2d6dc38b7387
Lucas Vitali <lucasvitali001@gmail.com>
2025-11-14 21:41:34 -0300

    feature: can use self on method definition

--- mini_smalltalk/src/Environment.py
+++ mini_smalltalk/src/Environment.py
@@ -228,21 +228,23 @@ class Environment:
             
         colaborators_rename, method_implementation, result = self.objects[receptor]["class_methods"][selector]
         
-        method_dictionary = {}
-        
+        method_dictionary = {"self": receptor}
+
         for idx in range(len(colaborators)):
             
             method_dictionary[colaborators_rename[idx]] = colaborators[idx]
         
         for message in method_implementation:
             
-            message_receptor, message_selector, message_colaborators_rename, message_result = message
+            message_receptor_rename, message_selector, message_colaborators_rename, message_result = message
             
             message_colaborators = []
             
             for idx in range(len(message_colaborators_rename)):
                 
                 message_colaborators.append(method_dictionary[message_colaborators_rename[idx]])
+
+            message_receptor = method_dictionary[message_receptor_rename]
             
             self.send_message(message_receptor, message_selector, message_colaborators, message_result, trace)
             
