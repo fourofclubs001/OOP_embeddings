042ad1092934d8e4c21c6d20de0c24073901f63b
Lucas Vitali <lucasvitali001@gmail.com>
2025-11-21 17:22:47 -0300

    feature: add pass test for trace with string new method

--- mini_smalltalk/tests/test_environments_utils.py
+++ mini_smalltalk/tests/test_environments_utils.py
@@ -130,49 +130,9 @@ class EnvironmentUtilsTest(unittest.TestCase):
 
     def test_can_trace_with_string_new_method(self):
 
-        self.environment.define_method("Dictionary", "create_identity_dictionary", ["key_and_value"],
-                                [("self", "set", ["key_and_value", "key_and_value"], "self")],
-                                "self")
+        trace = self.environment.send_message("String", "new", ["string_value"], "string", trace=True, base_case=True)
 
-        self.environment.define_method(
-                    "Dictionary", "create_identity_dictionary_with_reference", 
-                    ["reference_dictionary", "key_and_value_reference"],
-                    [("reference_dictionary", "get", ["key_and_value_reference"], "key_and_value_value"),
-                    ("self", "create_identity_dictionary", ["key_and_value_value"], "self")],
-                    "self"
-                )
-
-        self.environment.send_message("Class", "new", [], "UseCase")
-        self.environment.define_method(
-            
-            "UseCase", "use_case_00", [],
-            [
-                ("Dictionary", "new", [], "main_dictionary"),
-                ("String", "new", ["first_key"], "main_dictionary_key"),
-                ("Dictionary", "new", [], "reference_dictionary"),
-                ("String", "new", [""], "reference_dictionary_key"),
-                ("reference_dictionary", "set", 
-                ["reference_dictionary_key", "main_dictionary_key"], 
-                "reference_dictionary"),
-                ("main_dictionary", "create_identity_dictionary_with_reference",
-                ["reference_dictionary", "reference_dictionary_key"], "main_dictionary")
-            ],
-            "main_dictionary"
-        )
-
-        self.environment.send_message("UseCase", "new", [], "use_case")
-
-        trace = self.environment.send_message("use_case", "use_case_00", [], "use_case_result", trace=True, base_case=True)
-
-        expected_trace = [
-            (5, ('Class', 'new'), [], 14),
-            (3, ('Class', 'new'), ['first_key'], 16),
-            (5, ('Class', 'new'), [], 17),
-            (3, ('Class', 'new'), [''], 19),
-            (17, ('Dictionary', 'set'), [19, 16], 17),
-            (17, ('Dictionary', 'get'), [19], 16),
-            (14, ('Dictionary', 'set'), [16, 16], 14),
-            14
-        ]
-
-        self.assertEqual(trace, expected_trace)
+        self.assertEqual(trace[0][0], self.environment.objects["String"]["id"])
+        self.assertEqual(trace[0][1], ('Class', 'new'))
+        self.assertEqual(trace[0][2], ['string_value'])
+        self.assertEqual(trace[0][3], self.environment.objects["string"]["id"])
