0cd9b648c011b70e875a5a5a5f46ffbb35080a95
Lucas Vitali <lucasvitali001@gmail.com>
2025-11-22 18:53:34 -0300

    feature: define first dataset use case

--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -1,3 +1,63 @@
 from mini_smalltalk.src.Environment import Environment
 
 environment = Environment()
+
+initialization_trace = [
+    (
+        environment.objects["Class"]["id"], 
+        ("Class", "new"), 
+        [],
+        environment.objects["Class"]["id"]    
+    ),
+    (
+        environment.objects["Class"]["id"], 
+        ("Class", "new"), 
+        [],
+        environment.objects["String"]["id"]    
+    ),
+    (
+        environment.objects["Class"]["id"], 
+        ("Class", "new"), 
+        [],
+        environment.objects["Dictionary"]["id"]    
+    )
+]
+
+environment.send_message("Class", "new", [], "UseCase")
+
+environment.define_method(
+    "Dictionary", "get_two_values", 
+    ["first_key", "second_key"],
+    [
+        ("self", "get", ["first_key"], "first_value"),
+        ("self", "get", ["second_key"], "second_value"),
+        ("Dictionary", "new", [], "dictionary"),
+        ("dictionary", "set", ["first_key", "first_value"], "dictionary"),
+        ("dictionary", "set", ["second_key", "second_value"], "dictionary")
+    ], 
+    "dictionary")
+
+environment.define_method(
+    "UseCase", "use_case_00", [],
+    [
+        ("String", "new", ["first_main_key"], "first_main_key"),
+        ("String", "new", ["second_main_key"], "second_main_key"),
+        ("String", "new", ["first_main_value"], "first_main_value"),
+        ("String", "new", ["second_main_value"], "second_main_value"),
+        ("Dictionary", "new", [], "main_dictionary"),
+        ("main_dictionary", "set", ["first_main_key", "first_main_value"], "main_dictionary"),
+        ("main_dictionary", "set", ["second_main_key", "second_main_value"], "main_dictionary"),
+        ("main_dictionary", "get_two_values", ["first_main_key", "second_main_key"], "result_dictionary")
+    ],
+    "result_dictionary"
+)
+
+environment.send_message("UseCase", "new", [], "use_case")
+trace = environment.send_message("use_case", "use_case_00", [], "use_case_result", 
+                                 trace=True, base_case=True)
+
+final_trace = initialization_trace + trace
+
+for line in final_trace:
+
+    print(line)
